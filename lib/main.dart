fb60727e62432df8b12db55191fa4ee8831eeaf5
Ramadhan Pratama <tama110199@gmail.com>
2020-10-12 01:22:39 +0700

    tes aja hehe

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  // tes aja hehe
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
